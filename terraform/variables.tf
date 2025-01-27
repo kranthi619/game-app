92ef2b9abe81c39a1b86c688892cc0b77148a326
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2025-01-27 17:36:58 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,17 +1,62 @@
-variable "s3_bucket" {
-  description = "S3 bucket name for storing the Terraform state"
-  type        = string
-  default     = "my-eks-cluster-terraform-state-dev"
-}
-
-variable "s3_region" {
-  description = "Region of the S3 bucket"
+# AWS Region
+variable "region" {
+  description = "The AWS region to deploy resources in"
   type        = string
   default     = "us-east-1"
 }
 
-variable "dynamodb_table" {
-  description = "DynamoDB table name for Terraform state locking"
+# S3 Bucket for Terraform State
+variable "s3_bucket" {
+  description = "Name of the S3 bucket to store Terraform state"
   type        = string
-  default     = "my-eks-cluster-terraform-lock-dev"
+}
+
+# S3 Region for Terraform State
+variable "s3_region" {
+  description = "AWS region of the S3 bucket to store Terraform state"
+  type        = string
+}
+
+# EKS Cluster Name
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "game-2048-cluster"
+}
+
+# VPC ID
+variable "vpc_id" {
+  description = "VPC ID where the EKS cluster will be deployed"
+  type        = string
+}
+
+# Subnet IDs
+variable "subnet_ids" {
+  description = "List of subnet IDs for the EKS cluster"
+  type        = list(string)
+}
+
+# Node Group Configuration
+variable "desired_capacity" {
+  description = "Desired number of worker nodes in the EKS cluster"
+  type        = number
+  default     = 2
+}
+
+variable "max_capacity" {
+  description = "Maximum number of worker nodes in the EKS cluster"
+  type        = number
+  default     = 3
+}
+
+variable "min_capacity" {
+  description = "Minimum number of worker nodes in the EKS cluster"
+  type        = number
+  default     = 1
+}
+
+variable "instance_type" {
+  description = "Instance type for the EKS worker nodes"
+  type        = string
+  default     = "t2.medium"
 }
