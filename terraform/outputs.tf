e7bf36c9a997337d6c1a98bf8d5b4fe0733a5efd
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2025-01-27 17:19:03 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,20 @@
+output "cluster_id" {
+  description = "EKS cluster ID"
+  value       = module.eks.cluster_id
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_version" {
+  description = "EKS cluster version"
+  value       = module.eks.cluster_version
+}
+
+output "kubeconfig" {
+  description = "Kubeconfig file for the EKS cluster"
+  value       = module.eks.kubeconfig
+  sensitive   = true
+}
