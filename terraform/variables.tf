29851b8f14d7bf0fc70d322264bc1e839c04cd44
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2025-01-27 17:26:07 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,59 +1,17 @@
-variable "region" {
-  description = "AWS region to create resources in"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "s3_bucket" {
   description = "S3 bucket name for storing the Terraform state"
   type        = string
+  default     = "my-eks-cluster-terraform-state-dev"
 }
 
 variable "s3_region" {
   description = "Region of the S3 bucket"
   type        = string
+  default     = "us-east-1"
 }
 
 variable "dynamodb_table" {
   description = "DynamoDB table name for Terraform state locking"
   type        = string
-}
-
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-}
-
-variable "vpc_id" {
-  description = "VPC ID where the EKS cluster will be created"
-  type        = string
-}
-
-variable "subnet_ids" {
-  description = "List of subnet IDs for the EKS cluster"
-  type        = list(string)
-}
-
-variable "desired_capacity" {
-  description = "Desired number of nodes in the EKS node group"
-  type        = number
-  default     = 1
-}
-
-variable "max_capacity" {
-  description = "Maximum number of nodes in the EKS node group"
-  type        = number
-  default     = 2
-}
-
-variable "min_capacity" {
-  description = "Minimum number of nodes in the EKS node group"
-  type        = number
-  default     = 1
-}
-
-variable "instance_type" {
-  description = "Instance type for the EKS worker nodes"
-  type        = string
-  default     = "t2.medium"
+  default     = "my-eks-cluster-terraform-lock-dev"
 }
