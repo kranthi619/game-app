19ebd0d581bbdb1ba781509185f6e4144b5d303a
Kranthi Putti <120769579+kranthi619@users.noreply.github.com>
2025-01-27 17:37:23 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,20 +1,25 @@
-output "cluster_id" {
-  description = "EKS cluster ID"
-  value       = module.eks.cluster_id
+# Outputs for EKS Cluster
+output "eks_cluster_name" {
+  description = "The name of the EKS cluster"
+  value       = module.eks.cluster_name
 }
 
-output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
+output "eks_cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
   value       = module.eks.cluster_endpoint
 }
 
-output "cluster_version" {
-  description = "EKS cluster version"
-  value       = module.eks.cluster_version
+output "eks_cluster_security_group_id" {
+  description = "The security group ID for the EKS cluster"
+  value       = module.eks.cluster_security_group_id
 }
 
-output "kubeconfig" {
-  description = "Kubeconfig file for the EKS cluster"
-  value       = module.eks.kubeconfig
-  sensitive   = true
+output "node_group_arns" {
+  description = "ARNs of the node groups created for the EKS cluster"
+  value       = module.eks.node_groups_arn
+}
+
+output "node_group_names" {
+  description = "Names of the node groups created for the EKS cluster"
+  value       = module.eks.node_groups
 }
